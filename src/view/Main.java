51903823eb9440fc6e4a0943ddf47f1883b684da
Paulo Freitas <paulo.freitas.nt@gmail.com>
2023-11-01 07:58:14 -0300

    Adicionando leitura do arquivo

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,9 +1,6 @@
 package view;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.util.List;
 
 public class Main {
@@ -20,16 +17,29 @@ public class Main {
             }
         }
 
-        List<String> lista = List.of("João", "Maria", "Pedro");
+//        List<String> lista = List.of("João", "Maria", "Pedro");
 
         //Escrever no arquivo
+//        try{
+//            ObjectOutputStream out = new ObjectOutputStream(
+//                    new FileOutputStream(file)
+//            );
+//            out.writeObject(lista);
+//        }catch(IOException exception){
+//            System.out.println("Falha ao escrever no arquivo");
+//        }
+
+        //Ler do arquivo
         try{
-            ObjectOutputStream out = new ObjectOutputStream(
-                    new FileOutputStream(file)
+            ObjectInputStream in = new ObjectInputStream(
+                    new FileInputStream(file)
             );
-            out.writeObject(lista);
-        }catch(IOException exception){
-            System.out.println("Falha ao escrever no arquivo");
+            List<String> lista = (List<String>) in.readObject();
+            System.out.println(lista);
+        }catch (IOException exception){
+            System.out.println("Falha ao ler arquivo");
+        }catch (ClassNotFoundException e) {
+            System.out.println("Falha ao ler arquivo");
         }
 
     }
